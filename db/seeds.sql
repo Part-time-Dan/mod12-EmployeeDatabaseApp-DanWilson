80a566c0ac63d241705991f3294595e3cd684254
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-07-09 21:32:39 -0600

    sql schema  & seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,23 @@
 -- Test data tables will go here
+INSERT INTO department (name)
+VALUES  ("Management"),
+        ("Finance"),
+        ("Human Resources"),
+        ("Technology");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Manager", 150000.00, 1),
+        ("Accountant", 95000.00, 2),
+        ("Purchaser", 75000.00, 2),
+        ("HR Generalist", 60000.00, 3),
+        ("Software Engineer", 120000.00, 4),
+        ("Developer", 80000.00, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Big", "Boss", 1, null),
+        ("Second", "Person", 2, 1),
+        ("Someone", "Else", 3, 2),
+        ("Jane", "Doe", 5, null),
+        ("Guy", "Smiley", 6, 4),
+        ("Kind", "Stranger", 4, 1),
+        ("Snake", "Eyes", 6, 4);
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// run node app here
+// run command-line app here
 const inquirer = require('inquirer')
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
     //   public mysql password
-      password: '$3qualS3rv3r!',
+      password: process.env.DB_PASSWORD,
     //   add database name from schema.sql
-      database: ''
+      database: 'business_db'
     },
-    console.log(`Connected to the books_db database.`)
+    console.log(`Connected to the business_db database.`)
 );
